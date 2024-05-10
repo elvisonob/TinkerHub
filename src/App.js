15983b65311600c71342163961748a7380885204
elvisonob <elvisonob@yahoo.com>
2024-05-10 04:58:35 +0100

    setting layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import './App.css';
+
 const App = () => {
   return (
-    <div className="calculator">
-      <div>0</div>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
-      <div>6</div>
-      <div>7</div>
-      <div>8</div>
-      <div>9</div>
-      <div>+</div>
-      <div>-</div>
-      <div>/</div>
-      <div>=</div>
-      <div>x</div>
-      <div>C</div>
-      <div>Backspace</div>
+    <div className="wholePage">
+      <div className="calculator">
+        <div className="top1">top1</div>
+        <div className="top2">top2</div>
+        <div className="zero">0</div>
+        <div className="one">1</div>
+        <div className="two">2</div>
+        <div className="three">3</div>
+        <div className="four">4</div>
+        <div className="five">5</div>
+        <div className="six">6</div>
+        <div className="seven">7</div>
+        <div className="eight">8</div>
+        <div className="nine">9</div>
+        <div className="plus">+</div>
+        <div className="minus">-</div>
+        <div className="divide">/</div>
+        <div className="equalTo">=</div>
+        <div className="multiply">x</div>
+        <div className="C">C</div>
+        <div className="Backspace">Backspace</div>
+      </div>
     </div>
   );
 };
