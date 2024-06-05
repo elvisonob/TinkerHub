126d304ac7372d0cfb1339e5957fd296f43166df
elvisonob <elvisonob@yahoo.com>
2024-06-05 04:37:26 +0100

    CSS styled

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import './App.css';
-import React, { useState } from 'react';
 import Digits from './Digits';
 
 const App = () => {
   return (
-    <div className="wholePage">
-      <div className="calculator">
-        <div className="calculatorProper">
-          <div className="inputDisplay">
-            <div className="top1">top1</div>
-            <div className="top2">top2</div>
-          </div>
-          <Digits />
-        </div>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
       </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 };
