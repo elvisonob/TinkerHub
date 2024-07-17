d7c4e3a7cc09dd0566340ab5a23e966aca8d91d6
elvisonob <elvisonob@yahoo.com>
2024-07-17 05:02:34 +0100

    UserReducer form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
-import './App.css';
-import Digits from './Digits';
+import { useReducer } from 'react';
 
 const App = () => {
+  const reducerFn = (state, { type, payload }) => {
+    //take the content written in a form and console log it
+
+    switch (type) {
+      case 'addInput':
+        return { ...state, name: payload };
+
+      case 'addLocation':
+        return { ...state, location: payload };
+
+      case 'submit':
+        console.log(state.name, state.location);
+        return { ...state, name: '', location: '' };
+
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducerFn, {
+    name: '',
+    location: '',
+  });
+
+  const onNameInput = (e) => {
+    dispatch({ type: 'addInput', payload: e.target.value });
+  };
+
+  const onLocationInput = (e) => {
+    dispatch({ type: 'addLocation', payload: e.target.value });
+  };
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: 'submit' });
+  };
+
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand">1636383*</div>
-        <div className="current-operand">12940,49596</div>
-      </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
+    <div>
+      <h1>FORM</h1>
+      <form onSubmit={onHandleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          id="name"
+          password="name"
+          onChange={onNameInput}
+          value={state.name}
+        />
+        <label>Location:</label>
+        <input
+          type="text"
+          id="location"
+          name="location"
+          onChange={onLocationInput}
+          value={state.location}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
