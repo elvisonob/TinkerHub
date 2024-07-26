5b80505195fc935fd3188b087f05c245fe73f87b
elvisonob <elvisonob@yahoo.com>
2024-07-26 04:52:03 +0100

    Del button activated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const reducerFn = (state, { type, payload }) => {
         };
       }
 
-      if (state.previousOperand) {
+      if (state.previousOperand !== null) {
         return {
           ...state,
           currentOperand: `${state.currentOperand}${payload}`,
@@ -25,14 +25,27 @@ const reducerFn = (state, { type, payload }) => {
       return { currentOperand: `${state.currentOperand}${payload}` };
 
     case 'addOperation':
-      if (state.currentOperand) {
+      //if all states are null, operation key shouldn't show
+
+      //when i have all inputs and i click operator, previousOperand should evaluate
+
+      if (state.currentOperand !== null) {
         return {
           previousOperand: `${state.currentOperand}`,
           operator: payload,
           currentOperand: '',
         };
       }
-      return { operator: payload };
+
+      if (state.currentOperand && state.operator && state.previousOperand) {
+        return {
+          ...state,
+          operator: payload,
+          previousOperand: evaluate(state),
+        };
+      }
+
+      return state;
 
     case 'onEvaluate':
       return {
@@ -45,12 +58,15 @@ const reducerFn = (state, { type, payload }) => {
     case 'deleteEverything':
       return { currentOperand: '', operator: '', previousOperand: '' };
 
+    case 'delete':
+      return {
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+
     default:
       return state;
   }
 };
-
-// now i need to ensure that when equal-to button is clicked, it does the arithmetic
 
 const evaluate = ({ currentOperand, operator, previousOperand }) => {
   const prev = parseFloat(previousOperand);
@@ -105,7 +121,13 @@ const App = () => {
       <button className="span-two" onClick={onDelete}>
         AC
       </button>
-      <button>DEL</button>
+      <button
+        onClick={() => {
+          dispatch({ type: 'delete' });
+        }}
+      >
+        DEL
+      </button>
       <OperationDigits operation="/" dispatch={dispatch} />
       <Digits numbers="1" dispatch={dispatch} />
       <Digits numbers="2" dispatch={dispatch} />
