98c664cc89e7c903f8757bdaa812e446ef6c26f3
elvisonob <elvisonob@yahoo.com>
2024-07-28 08:55:38 +0100

    brain-storming

--- src/App.js
+++ src/App.js
@@ -7,25 +7,12 @@ import OperationDigits from './OperationDigits';
 const reducerFn = (state, { type, payload }) => {
   switch (type) {
     case 'addFigure':
-      // when blank, do not show operator!!
-
       if (payload.numbers === '0' && state.currentOperand === '0') {
         return state;
       }
 
-      //
-
-      if (state.currentOperand.includes('.') && payload.numbers === '.') {
+      if (payload.numbers === '.' && state.currentOperand.includes('.')) {
         return state;
-      }
-
-      // if state.currentoperand already includes '0.' and next payload is 0, include return state
-
-      if (state.previousOperand !== null) {
-        return {
-          ...state,
-          currentOperand: `${state.currentOperand}${payload.numbers}`,
-        };
       }
 
       return {
@@ -34,87 +21,30 @@ const reducerFn = (state, { type, payload }) => {
       };
 
     case 'addOperation':
-      //if all states are null, operation key shouldn't show
-
-      if (state.currentOperand === null && state.previousOperand === null) {
+      if (state.currentOperand == null && state.previousOperand == null) {
         return state;
       }
 
-      //when i have all inputs and i click operator, previousOperand should evaluate
-
-      // if (state.currentOperand !== null) {
-      //   return {
-      //     previousOperand: `${state.currentOperand}`,
-      //     operator: payload.operation,
-      //     currentOperand: '',
-      //   };
-      // }
-
-      // if (
-      //   state.currentOperand !== null &&
-      //   state.operator !== null &&
-      //   state.previousOperand !== null
-      // ) {
-      //   return {
-      //     ...state,
-      //     operator: payload.operation,
-      //     previousOperand: evaluate(state),
-      //   };
-      // }
-
-      return state;
-
-    case 'onEvaluate':
-      return {
-        ...state,
-        previousOperand: null,
-        operator: null,
-        currentOperand: evaluate(state),
-      };
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operator: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        };
+      }
 
     case 'deleteEverything':
-      return { currentOperand: '', operator: '', previousOperand: '' };
+      return {};
 
-    case 'delete':
-      return {
-        currentOperand: state.currentOperand.slice(0, -1),
-      };
-
-    default:
-      return state;
+    // default:
+    //   return state;
   }
-};
-
-const evaluate = ({ currentOperand, operator, previousOperand }) => {
-  const prev = parseFloat(previousOperand);
-  const current = parseFloat(currentOperand);
-  if (isNaN(prev) || isNaN(current)) return '';
-  let computation = '';
-  switch (operator) {
-    case '+':
-      computation = prev + current;
-      break;
-
-    case '-':
-      computation = prev - current;
-      break;
-
-    case '/':
-      computation = prev / current;
-      break;
-
-    case '*':
-      computation = prev * current;
-      break;
-    default:
-      return '';
-  }
-  return computation.toString();
 };
 
 const App = () => {
   const [state, dispatch] = useReducer(reducerFn, {
-    currentOperand: '',
+    currentOperand: 'J',
     operator: '',
     previousOperand: '',
   });
