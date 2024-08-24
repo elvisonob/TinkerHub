5274cb9b9fbf243d9f05848591fc5d135829ed3b
elvisonob <elvisonob@yahoo.com>
2024-08-24 08:17:00 +0100

    more saving

--- src/App.js
+++ src/App.js
@@ -1,44 +1,17 @@
-import React, { useState } from 'react';
 import './App.css';
 import InputTodo from './InputTodo';
 import ListOfTodos from './ListOfTodo';
-import { TodoCtx } from './store/Context';
+import { TodoCtxProvider } from './store/Context';
 
 const App = () => {
-  const [finalTodoList, setFinalTodoList] = useState([]);
-
-  const onAddTodo = (userText) => {
-    const newTodo = {
-      id: Date.now(),
-      text: userText,
-    };
-
-    console.log(newTodo.id);
-    setFinalTodoList((prevTodo) => {
-      return [...prevTodo, newTodo];
-    });
-  };
-
-  const removeTodo = (id) => {
-    setFinalTodoList((prevTodo) => {
-      return prevTodo.filter((eachTodo) => eachTodo.id !== id);
-    });
-  };
-
   return (
-    <TodoCtx.Provider
-      value={{
-        todoList: finalTodoList,
-        removeTodo: removeTodo,
-        onAddTodo: onAddTodo,
-      }}
-    >
+    <TodoCtxProvider>
       <div>
         <h1>TODO APP</h1>
         <InputTodo />
         <ListOfTodos />
       </div>
-    </TodoCtx.Provider>
+    </TodoCtxProvider>
   );
 };
 
--- src/store/Context.js
+++ src/store/Context.js
@@ -1,6 +1,36 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 export const TodoCtx = createContext({
   todoList: [],
   removeTodo: () => {},
 });
+
+export const TodoCtxProvider = ({ children }) => {
+  const [finalTodoList, setFinalTodoList] = useState([]);
+
+  const onAddTodo = (userText) => {
+    const newTodo = {
+      id: Date.now(),
+      text: userText,
+    };
+
+    console.log(newTodo.id);
+    setFinalTodoList((prevTodo) => {
+      return [...prevTodo, newTodo];
+    });
+  };
+
+  const removeTodo = (id) => {
+    setFinalTodoList((prevTodo) => {
+      return prevTodo.filter((eachTodo) => eachTodo.id !== id);
+    });
+  };
+
+  const allCtx = {
+    todoList: finalTodoList,
+    removeTodo: removeTodo,
+    onAddTodo: onAddTodo,
+  };
+
+  return <TodoCtx.Provider value={allCtx}>{children}</TodoCtx.Provider>;
+};
