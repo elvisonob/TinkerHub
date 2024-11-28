a22834e734785678ffdb23ab7a5f4556f6873c18
elvisonob <elvisonob@yahoo.com>
2024-11-28 04:05:22 +0000

    Insightful summation

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,17 @@ const counter = createSlice({
     },
 
     increaseBy20(state, action) {
-      state.counter = state.counter + action.payload;
+      //if counter is less than 20, increase, and if counter
+      // is more than 20, do not go past 20
+      if (state.counter >= 20) {
+        return;
+      }
+
+      if (state.counter + action.payload > 20) {
+        state.counter = 20;
+      } else {
+        state.counter = state.counter + action.payload;
+      }
     },
   },
 });
