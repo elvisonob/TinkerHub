5eb077f7b71ac83db17c64a0451213801f7abe93
elvisonob <elvisonob@yahoo.com>
2024-12-04 07:46:51 +0000

    cracking the codes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,31 @@
+import React, { useState, useEffect } from 'react';
+
 const TodoList = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:5000/api/todo/');
+
+    if (!response.ok) {
+      console.log('Https not found');
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h2>List of added Todo</h2>
       <ul>
-        <li>Added todo</li>
+        {todoList.map((eachItem) => (
+          <li key={eachItem.id}>{eachItem.text}</li>
+        ))}
       </ul>
     </div>
   );
