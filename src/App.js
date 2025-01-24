9e280d2ec61e4b35d75986181fc32f2e8ed0d4b4
elvisonob <elvisonob@yahoo.com>
2025-01-24 08:11:33 +0000

    Rebuilding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
 
-const App = () => {
-  return (
-    <div>
-      <TodoForm />
-      <TodoList />
-    </div>
-  );
-};
-
-export default App;
