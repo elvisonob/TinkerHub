3626a3d46ea766d41ff0e0f7430e813c778584d7
elvisonob <elvisonob@yahoo.com>
2025-02-15 10:54:59 +0000

    moving state down

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,7 +1,15 @@
 import QuestionTimer from './QuestionTimer';
 import Answers from './Answers.jsx';
+import { useState } from 'react';
+import QUESTIONS from '../questions';
+
+/*
+Set the answer state here, check if an answer is right or wrong
+or whether an answer has been selected 
+*/
 
 const Questions = ({
+  key,
   questions,
   selectAnswer,
   onSelect,
@@ -9,6 +17,28 @@ const Questions = ({
   onSkipAnswer,
   questionText,
 }) => {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null,
+  });
+
+  const handleSelectAnswer = (answer) => {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    // setTimeout to discover whether it is the right answer or wrong one
+
+    let answerState = '';
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[key].answers[0] === answer,
+      });
+    }, 1000);
+  };
   return (
     <div id="question">
       <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
@@ -16,7 +46,7 @@ const Questions = ({
       <Answers
         userSelectAnswer={selectAnswer}
         answerState={answerState}
-        onSelect={onSelect}
+        onSelect={handleSelectAnswer}
         question={questions}
       />
     </div>
