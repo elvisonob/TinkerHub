87843e37f6c7e3c69ec35fa31b79ede2d366c4e8
elvisonob <elvisonob@yahoo.com>
2025-05-22 08:55:09 -0700

    tinkering commences

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,43 +1,28 @@
-import { useState, useCallback } from 'react';
-import Summary from './Summary.js';
+import { useState } from 'react';
 import QUESTIONS from '../questions.js';
-import QuestionAnswers from './QuestionAnswers.js';
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
-  console.log(userAnswers);
-
   const activeQuestionIndex = userAnswers.length;
 
-  const onhandleAnswerClick = useCallback((answer) => {
-    setUserAnswers((prev) => {
-      return [...prev, answer];
-    });
-  }, []);
-
-  const onSkipAnswer = useCallback(() => {
-    onhandleAnswerClick(null);
-  }, [onhandleAnswerClick]);
-
-  if (activeQuestionIndex === QUESTIONS.length) {
-    return (
-      <div>
-        <h1>
-          <Summary />
-        </h1>
-      </div>
-    );
-  }
-
+  const onhandleAnswerClick = (answer) => {
+    setUserAnswers((prev) => [...prev, answer]);
+  };
   return (
     <div className="container">
-      <QuestionAnswers
-        key={activeQuestionIndex}
-        index={activeQuestionIndex}
-        onhandleAnswerClick={onhandleAnswerClick}
-        onSkipAnswer={onSkipAnswer}
-      />
+      <div className="quiz">
+        <div>
+          <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+          <ul>
+            {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+              <li key={answer} onClick={() => onhandleAnswerClick(answer)}>
+                {answer}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
