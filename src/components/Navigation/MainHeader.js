90a5bf418bfc73b7594bec49f701ce59e308c9dd
elvisonob <elvisonob@yahoo.com>
2025-07-05 09:46:08 -0700

    adding React Portal

--- src/components/Navigation/MainHeader.js
+++ src/components/Navigation/MainHeader.js
@@ -2,19 +2,24 @@ import MainNavigation from './MainNavigation';
 import classes from './MainHeader.module.css';
 import { Fragment } from 'react';
 import SideDrawer from './SideDrawer';
+import { createPortal } from 'react-dom';
 
 const MainHeader = () => {
   return (
     <Fragment>
-      {/* <SideDrawer>
-        <nav className={classes['menu-button__mobile']}>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Portfolio</li>
-          </ul>
-        </nav>
-      </SideDrawer> */}
+      {createPortal(
+        <SideDrawer>
+          <nav className={classes['menu-button__mobile']}>
+            <ul className={classes['mobile-navlinks']}>
+              <li>Home</li>
+              <li>About</li>
+              <li>Portfolio</li>
+            </ul>
+          </nav>
+        </SideDrawer>,
+        document.getElementById('sideDrawer-Portal')
+      )}
+
       <div className={classes['main-header']}>
         <button className={classes['menu-button__span']}>
           <span />
