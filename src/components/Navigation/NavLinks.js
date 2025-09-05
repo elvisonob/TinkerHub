f7ee3e82efdec2da8cf6486233aff669dad0fe46
elvisonob <elvisonob@yahoo.com>
2025-09-05 14:58:07 +0100

    adding

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -4,7 +4,7 @@ const NavLinks = (props) => {
   return (
     <ul className={classes['nav-link']}>
       <li>Home</li>
-      <li>About</li>
+      <li>About Elvis</li>
       <li>Portfolio</li>
     </ul>
   );
