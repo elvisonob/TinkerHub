e27511492a714c0ffaa9b3fc0a02175370c1efc8
elvisonob <elvisonob@yahoo.com>
2025-09-11 13:58:46 +0100

    textchange

--- src/components/Navigation/NavLinks.js
+++ src/components/Navigation/NavLinks.js
@@ -3,7 +3,7 @@ import classes from './NavLinks.module.css';
 const NavLinks = (props) => {
   return (
     <ul className={classes['nav-link']}>
-      <li>Home</li>
+      <li>HomePage</li>
       <li>About</li>
       <li>Portfolio 2</li>
     </ul>
