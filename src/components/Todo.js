5a78df6a53fb0ef0d48c13166265fd5aba3a7168
elvisonob <elvisonob@yahoo.com>
2025-10-29 05:02:30 +0000

    text add

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,26 +1,35 @@
 import { useState, useEffect } from 'react';
 
 const Todo = () => {
-  const [directory, setDirectory] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/users');
-      const data = await res.json();
-      setDirectory(data);
-    };
+  const [text, setText] = useState('');
+  const [todoList, setTodoList] = useState([]);
 
-    fetchData();
-  }, []);
+  const onInput = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setTodoList((prev) => {
+      return [...prev, { id: Math.random(), todo: text }];
+    });
+    setText('');
+  };
+
   return (
     <div className="container">
-      <h1>API Data fetched</h1>
-      <div className="data-box">
-        {directory.map((dir) => (
-          <div className="card">
-            <p>Name:{dir.name}</p>
-            <p>Email:{dir.email}</p>
-            <p>Website:{dir.website}</p>
-          </div>
+      <h1>TODO APP</h1>
+      <div>Add a TodoList</div>
+      <div>
+        <form onClick={onSubmit}>
+          <input type="text" id="text" onChange={onInput} value={text} />
+          <button>ENTER</button>
+        </form>
+      </div>
+      <div>
+        <p>List of todos</p>
+        {todoList.map((todo) => (
+          <div key={todo.id}>{todo.todo}</div>
         ))}
       </div>
     </div>
