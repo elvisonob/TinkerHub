fc3c84f4d59340ffcb5c3fcf76c0e82d8cf8c448
elvisonob <elvisonob@yahoo.com>
2026-01-18 11:44:15 +0000

    MUI added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Todo from './components/Todo.js';
 //import AreaChartComponent from './components/AreaChart';
+import Mui from './components/MUIfeature.js';
 
 const App = () => {
   return (
     <div>
-      <Todo />
+      <Mui />
     </div>
   );
 };
---
+++ src/components/MUIfeature.js
@@ -0,0 +1,10 @@
+import classes from './MUIfeature.module.css';
+import Button from '@mui/material/Button';
+
+// export default function Mui() {
+//   return <button className={classes.btn}>Click Me</button>;
+// }
+
+export default function Mui() {
+  return <Button variant="contained">Click Me</Button>;
+}
