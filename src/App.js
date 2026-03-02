acc3f336d7701114074c325b3fa824058bb242de
elvisonob <elvisonob@yahoo.com>
2026-03-02 06:21:48 +0000

    practice

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
 import { useForm } from 'react-hook-form';
-import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { minLength, z } from 'zod';
 
 const App = () => {
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      email: 'elvisIsKing@yahoo.com',
+    },
+  });
 
   const onSubmit = async (data) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     console.log(data);
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        {...register('email', {
-          required: 'Email must be present',
-          pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-        })}
-        type="text"
-        placeholder="Email"
-      />
-      {errors.email && <p>{errors.email.message}</p>}
-      <input
-        {...register('password', {
-          required: 'Password must be added',
-          minLength: {
-            value: 8,
-            message: 'Password must have at least 8 characters',
-          },
-        })}
-        type="password"
-        placeholder="Password"
-      />
-      {errors.password && <p>{errors.password.message}</p>}
-      <button type="submit">{isSubmitting ? 'Loading...' : 'Submit'}</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Email</label>
+        <input
+          type="text"
+          {...register('email', {
+            required: 'Email must be added',
+            validate: (value) => {
+              if (!value.includes('@')) {
+                return 'Email must include @';
+              }
+              return true;
+            },
+          })}
+          placeholder="email"
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+
+        <label>Password</label>
+        <input
+          type="password"
+          {...register('password', {
+            required: 'Password must be added',
+          })}
+          placeholder="password"
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+        <button>{isSubmitting ? 'Loading...' : 'Submit'}</button>
+      </form>
+    </div>
   );
 };
 
