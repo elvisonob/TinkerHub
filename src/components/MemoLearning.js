35305bcd1b9aff4f3cc632663448e8e8a83f7559
elvisonob <elvisonob@yahoo.com>
2026-03-23 06:22:20 +0000

    Abort Controller

--- src/components/MemoLearning.js
+++ src/components/MemoLearning.js
@@ -36,7 +36,7 @@ export default function MemoLearning() {
   return (
     <>
       <h1>Hello</h1>
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading..</p>}
     </>
   );
 }
