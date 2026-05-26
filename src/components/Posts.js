8c3e762093b4f1c88a64bca319346f3264247e2e
elvisonob <elvisonob@yahoo.com>
2026-05-26 04:39:23 +0100

    coding

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,16 +1,1 @@
-import React from 'react';
 
-const Posts = ({ posts, loading }) => {
-  if (loading) {
-    return <h2>Loading...</h2>;
-  }
-  return (
-    <ul>
-      {posts.map((post) => (
-        <li key={post.title}>{post.title}</li>
-      ))}
-    </ul>
-  );
-};
-
-export default Posts;
--- src/components/UseCallBack.js
+++ src/components/UseCallBack.js
@@ -3,37 +3,12 @@ import Posts from './Posts';
 import Pagination from './Pagination';
 import axios from 'axios';
 
-export default function UseCallBack() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      setPosts(res.data);
-      setLoading(false);
-    };
-    fetchPosts();
-  }, []);
-
-  // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+function UseCallBack() {
   return (
-    <div className="container">
-      <h1>My Blog</h1>
-      <Posts posts={currentPosts} loading={loading} />
-      <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
-        paginate={paginate}
-      />
+    <div>
+      <h1>Hello Motor</h1>
     </div>
   );
 }
+
+export default UseCallBack;
